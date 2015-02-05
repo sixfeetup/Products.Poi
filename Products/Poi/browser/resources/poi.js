e90f9eb84b5bdd84cc58b4ede31893c0b624f03f
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2015-02-04 21:45:13 -0500

    added full search result sorting that does more than just the first batch that is one the screen

--- Products/Poi/browser/resources/poi.js
+++ Products/Poi/browser/resources/poi.js
@@ -12,4 +12,43 @@ jQuery(function($) {
             closeselector: '[name=form.buttons.cancel]',
         });
     }
+
+    // parse the current querystring
+    var qsmap = URI(location).query(true);
+    var headers = $('.issue-search-results thead th');
+    // map headers to sort types
+    var sortheaders = {
+        0 : 'id',
+        1 : 'sortable_title',
+        2 : 'getRelease',
+        3 : 'getArea',
+        4 : 'getIssueType',
+        5 : 'getSeverity',
+        6 : 'getResponsibleManager',
+        7 : '',
+        8 : 'review_state',
+    }
+    // loop the headers and attach links
+    for (var i = 0; i < headers.length; i++) {
+        var newuri = URI(location);
+        newuri.search(function(data) {
+            data.sort_on = sortheaders[i];
+            data.sort_order = 'ascending';
+        });
+        $(headers[i]).wrapInner(function() {
+            var newheader = $("<div><a></a></div>");
+            if (qsmap.hasOwnProperty('sort_on') && qsmap.sort_on == sortheaders[i]) {
+                $(newheader).attr('class','sorted-header');
+                newuri.search(function(data) {
+                    data.sort_order = 'descending';
+                });
+                if (qsmap.hasOwnProperty('sort_order') && qsmap.sort_order == 'ascending')
+                    $(newheader).append('<img src="arrowUp.png"/>');
+                else
+                    $(newheader).append('<img src="arrowDown.png"/>');
+            };
+            $('a', newheader).attr('href', newuri);
+            return newheader;
+        });
+    }
 });
